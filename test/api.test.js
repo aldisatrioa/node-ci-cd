a80c2f8691fa9b514ab261643110e8ed01bc3969
Aldi <aldisa546@gmail.com>
2021-01-26 20:47:43 +0700

    should fail

--- test/api.test.js
+++ test/api.test.js
@@ -4,7 +4,7 @@ const request = require('supertest')
 describe('Task API', () => {
     it('it should fetch all', async (done) => {
         const result = await request(app).get("/api/task")
-        expect(result.statusCode).toBe(201)
+        expect(result.statusCode).toBe(1)
 
         done()
     })
